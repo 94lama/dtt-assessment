57c1893680af21693e25ba429929c4dc48729c4d
94lama <riccardo.lamalfa94@gmail.com>
2024-07-11 21:28:46 +0200

    first push

---
+++ src/components/Navbar.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+</script>
+
+<template>
+  <nav>
+    <div class="navbar">
+        <img class="logo" src="/img_logo_dtt@3x.png" alt="DTT logo" />
+        <RouterLink to="/" >Houses</RouterLink>
+        <RouterLink to="/about" >About</RouterLink>
+    </div>
+</nav>
+</template>
+
+<style scoped> 
+nav {
+    position: absolute;
+    display: flex;
+    top: 0;
+    left: 0;
+    width: 100%;
+    background-color: var(--background2);
+    justify-content: center;
+    align-items: center;
+}
+
+.navbar {
+    width: var(--width);
+    height: 40px;
+    display: flex;
+    justify-content: left;
+    align-items: center;
+    padding: 10px;
+    column-gap: 40px;
+}
+
+.router-link-active{
+    color: var(--text-primary);
+}
+
+.logo {
+    height: 100%;
+};
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { computed } from 'vue'
+import { useFetch } from '@vueuse/core';
+
+const { isFetching, data, error } = useFetch('https://api.intern.d-tt.nl/api/houses', {
+  method: 'GET',
+  headers: { 
+    'X-Api-Key': '1owkyeAxJYWi-8EjqbgQ5IK_OLT2VGHC'
+   },
+});
+
+const houses = computed(() => JSON.parse(data.value));
+</script>
+
+<template>
+  <div v-if="isFetching">
+    Loading...
+  </div>
+  <div v-else-if="data">
+    {{ houses }}
+  </div>
+  <div v-else>
+    {{ error }}
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
